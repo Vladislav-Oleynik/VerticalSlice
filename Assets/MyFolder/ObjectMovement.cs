46c7fbb4b52b12ee5705e7ba33707a4ed95fde2b
Vladislav-Oleynik <kaktuskolushiy@gmail.com>
2020-11-07 01:27:06 +0200

    first commit

---
+++ Assets/MyFolder/ObjectMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    public float radius = 2f;
+    public float angle = 0f;
+    public float speed = 2f;
+
+    void Update()
+    {
+        angle += Time.deltaTime;
+        var x = Mathf.Cos(angle * speed) * radius;
+        var z = Mathf.Sin(angle * speed) * radius;
+        transform.position = new Vector3(x, 0.5f, z);
+    }
+}
---
+++ Assets/MyFolder/Waypoints.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public float speed = 5f;
+    public Vector3 target;
+    public int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = waypoints[i].transform.position;
+        transform.Translate(Vector3.Normalize(target - transform.position) * Time.deltaTime * speed);
+        float distance = Vector3.Distance(target, transform.position);
+
+        if (distance < 0.5f)
+            i++;
+
+        if (i > 3)
+            i = 0;
+    }
+}
